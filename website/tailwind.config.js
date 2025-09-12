60f8fea788b5c67064822736b54e234468a4cc6b
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-09-12 22:30:08 +0330

    config file tailwind.config.js

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,11 +2,48 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgBlackHigh: '#0a0a0a',
+        bgBlackLow: '#171717',
+        textActive: '#e5e8e1',
+        textNotActive: '#a1a1a1',
+      },
+
+      spacing: {
+        xs: '4px',
+        sm: '8px',
+        md: '16px',
+        lg: '24px',
+        xl: '32px',
+        '2xl': '48px',
+        '3xl': '64px',
+      },
+
+      fontSize: {
+        xs: ['12px', '16px'],
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['18px', '28px'],
+        xl: ['20px', '32px'],
+        '2xl': ['24px', '36px'],
+        '3xl': ['30px', '44px'],
+        '4xl': ['36px', '48px'],
+      },
+
+      borderRadius: {
+        none: '0',
+        sm: '4px',
+        md: '8px',
+        lg: '12px',
+        xl: '16px',
+        '2xl': '24px',
+        '3xl': '32px',
+        full: '9999px',
+      },
+    },
   },
   plugins: [],
 };
