811df0045055cb728513116b5f704ef5f3a1a130
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-09-15 13:16:42 +0330

    config hero-section

--- website/src/components/download/hero-section.tsx
+++ website/src/components/download/hero-section.tsx
@@ -3,17 +3,26 @@ import Image from "next/image";
 
 function HeroSection() {
   return (
-    <div className="flex justify-center items-center ">
-      <div className="relative w-[95%] h-[800px] mt-[100px]">
+    <section className="flex justify-center items-center mt-24">
+      <div className="relative w-[95%] h-[835px]">
         <Image
           src="/images/gradient-hero-prerender.avif"
           alt="Gradient hero background"
           fill
           priority
-          className="rounded-2xl object-cover object-center"
+          className="rounded-xl object-cover object-center"
         />
+
+        <div className="absolute inset-0 flex flex-col justify-start items-center mt-20 z-10 text-center">
+          <h1 className="text-textActive text-[120px] font-bold">
+            The AI Music Player
+          </h1>
+          <p className="text-textActive text-xl font-semibold">
+            Synapse makes listening immersive with AI — first made for Mark Zuckerberg.
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
