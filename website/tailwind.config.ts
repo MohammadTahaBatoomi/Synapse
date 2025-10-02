83e1d2bcb3cdeb337e9c47498a25d6594dc380a2
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-02 18:45:17 +0330

    update tailwind.config.css

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -6,31 +6,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bgBlackHigh: '#0a0a0a',
-        bgBlackLow: '#171717',
-        textActive: '#e5e8e1',
-        textNotActive: '#a1a1a1',
-      },
-
-      spacing: {
-        xs: '4px',
-        sm: '8px',
-        md: '16px',
-        lg: '24px',
-        xl: '32px',
-        '2xl': '48px',
-        '3xl': '64px',
-      },
-
-      borderRadius: {
-        none: '0',
-        sm: '4px',
-        md: '8px',
-        lg: '12px',
-        xl: '16px',
-        '2xl': '24px',
-        '3xl': '32px',
-        full: '9999px',
+        deepforest: '#394508',
+        limemist: '#D2FD9C',
+        olivebranch: '#619111',
+        olivedrab: '#77912A',
+        stonegray: '#5D5D5D',
       },
     },
   },
