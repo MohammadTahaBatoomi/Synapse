3d5ac1713aca3de29885f75f57a9d6e79cc85ee1
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-02 19:57:02 +0330

    code sidebar

--- website/src/components/shared/Shared-Sidebar.tsx
+++ website/src/components/shared/Shared-Sidebar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="bg-deepforest h-screen w-[25%] p-8">
+      <div className="flex gap-4 items-center">
+        <Image
+          src="/images/logo.svg"
+          alt="Logo MyWebsite"
+          className="rotate-180 h-4 w-4"
+          width={0}
+          height={0}
+        />
+        <p className="text-white font-bold">Synapse</p>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
