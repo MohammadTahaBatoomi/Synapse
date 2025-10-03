06060e79ae9ad5e7a444ec60027e3b5049ba9b1d
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-03 18:58:24 +0330

    code & config Get-More-Week and test and config page for imported componentes

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import Sidebar from '@/components/landing/Sidebar'
-import HeroSection from '@/components/landing/Hero-Section'
-import SharedText from '@/components/shared/Shared-Text'
+import React from "react";
+import Sidebar from "@/components/landing/Sidebar";
+import HeroSection from "@/components/landing/Hero-Section";
+import SharedText from "@/components/shared/Shared-Text";
+import GetMoreWeek from "@/components/landing/Get-More-Week";
+import { IoStatsChartSharp } from "react-icons/io5";
+import { AiFillDatabase } from "react-icons/ai";
 function Page() {
   return (
     <div className="flex flex-col xl:flex-row w-full min-h-screen">
@@ -9,11 +12,26 @@ function Page() {
 
       <main className="flex-1 xl:ml-[480px]">
         <HeroSection />
-        <SharedText variant='up' text='Get more done in a week' className='mt-10' />
-        <SharedText variant='down' text='Maximize your productivity with smarter tools designed to streamline your workflow to automate tasks, stay organized' />
+        <SharedText
+          variant="up"
+          text="Get more done in a week"
+          className="mt-10"
+        />
+        <SharedText
+          variant="down"
+          text="Maximize your productivity with smarter tools designed to streamline your workflow to automate tasks, stay organized"
+        />
+        <div className="flex justify-center items-center mt-5 ">
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+            <GetMoreWeek text="Double Your Productivity" icon="2x" />
+            <GetMoreWeek text="Efficiency Increase Per Transfer" icon={<IoStatsChartSharp />} />
+            <GetMoreWeek text="Centralize Your Finances" icon={<AiFillDatabase />} />
+            <GetMoreWeek text="More Activity" icon="130%" />
+          </div>
+        </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
--- website/src/components/landing/Get-More-Week.tsx
+++ website/src/components/landing/Get-More-Week.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client";
 
-function GetMoreWeek() {
-  return (
-    <div>GetMoreWeek</div>
-  )
+import React from "react";
+
+interface DynamicBoxProps {
+  icon: React.ReactNode;
+  text: string;
+  onClick?: () => void;
+  className?: string;
 }
 
-export default GetMoreWeek
+const GetMoreWeek: React.FC<DynamicBoxProps> = ({ icon, text, onClick, className }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`w-auto h-[200px] flex flex-col items-center justify-center xl:py-6 py-8 px-[100px] 
+                  rounded-2xl cursor-pointer transition-all text-center
+                  bg-limemist 
+                  ${className || ""}`}
+    >
+      <div className="text-7xl text-deepforest font-medium mb-4">{icon}</div>
+      <p className="mt-2 text-deepfores text-lg">{text}</p>
+    </div>
+  );
+};
+
+export default GetMoreWeek;
+
+  
