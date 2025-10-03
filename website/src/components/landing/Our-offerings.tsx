19affbf934b806186eff1536a1d6d0bb18327dca
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-03 06:48:59 +0330

    code Our-Offerings and tested

--- website/src/components/landing/Our-offerings.tsx
+++ website/src/components/landing/Our-offerings.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
 
-function Ourofferings() {
-  return (
-    <div>Ourofferings</div>
-  )
+import React from "react";
+
+interface DynamicBoxProps {
+  icon: React.ReactNode;
+  text: string;
+  onClick?: () => void;
+  className?: string;
 }
 
-export default Ourofferings
+const Ouroffeerning: React.FC<DynamicBoxProps> = ({ icon, text, onClick, className }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`w-48 h-48 flex flex-col items-center justify-center 
+                  rounded-lg cursor-pointer transition-all 
+                  bg-olivebranch hover:bg-limemist 
+                  ${className || ""}`}
+    >
+      <div className="text-4xl">{icon}</div>
+      <p className="mt-2 font-medium">{text}</p>
+    </div>
+  );
+};
+
+export default Ouroffeerning;
