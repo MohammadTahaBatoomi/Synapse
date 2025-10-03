2bddb1f113647ad9f86bf71cbc074405faa6a7f5
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-03 05:54:34 +0330

    1 step code & cofig in sidebar.tsx

--- website/src/components/shared/Shared-Sidebar.tsx
+++ website/src/components/shared/Shared-Sidebar.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import DynamicButton from "./Shared-Button";
 
 function Sidebar() {
   return (
     <div className="bg-deepforest h-screen w-[25%] p-8">
-      <div className="flex gap-4 items-center">
-        <Image
-          src="/images/logo.svg"
-          alt="Logo MyWebsite"
-          className="rotate-180 h-4 w-4"
-          width={0}
-          height={0}
-        />
-        <p className="text-white font-bold">Synapse</p>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4 items-center">
+          <Image
+            src="/images/logo.svg"
+            alt="Logo MyWebsite"
+            className="rotate-180 h-4 w-4"
+            width={0}
+            height={0}
+          />
+          <p className="text-white font-bold">Synapse</p>
+        </div>
+        <DynamicButton text="Get Started"/>
       </div>
     </div>
   );
