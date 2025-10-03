12cedbfdcd22ad01a1c0dd73a56572e3b2c3760b
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-03 17:52:42 +0330

    code & config Hero-Section

--- website/src/components/landing/Hero-Section.tsx
+++ website/src/components/landing/Hero-Section.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 function HeroSection() {
   return (
-    <div className="flex justify-center items-center p-4">
+    <div className="flex justify-center items-center p-4 mt-8">
       <Image
         src="/images/4dd369a6b1dd2d9d8c3e5227ef4f51e7f941a6ba.png"
         alt="hero-section img"
-        width={1200}    
-        height={1000}
-        className="rounded-lg object-contain"
+        width={1350}    
+        height={0}
+        className="max-h-[650px] object-cover rounded-t-3xl"
       />
     </div>
   );
