6ff41a56294e68b81394fdbbe3d571fd5fe07f91
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-03 17:54:32 +0330

    code and config Shared-Text-Up.tsx

--- website/src/components/shared/Shared-Text-Up.tsx
+++ website/src/components/shared/Shared-Text-Up.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
-function SharedText() {
-  return (
-    <div>SharedText</div>
-  )
+interface SharedTextProps {
+  text: string;
+  size?: string; 
+  color?: string;
+  weight?: string;
+  align?: "left" | "center" | "right";
+  className?: string; 
 }
 
-export default SharedText
+function SharedText({
+  text,
+  size = "text-base",
+  color = "text-gray-800",
+  weight = "font-normal",
+  align = "left",
+  className = "",
+}: SharedTextProps) {
+  return (
+    <p className={`${size} ${color} ${weight} text-${align} ${className}`}>
+      {text}
+    </p>
+  );
+}
+
+export default SharedText;
