a3dc3bb605d1eb395e83f1f97ad77dd709316f0b
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-10-04 18:16:04 +0330

    convert hero-section for Carousel-banner

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "@/components/landing/Sidebar";
-import HeroSection from "@/components/landing/Hero-Section";
+import CarouselBanner from "@/components/landing/Carousel-Banner";
 import SharedText from "@/components/shared/Shared-Text";
 import GetMoreWeek from "@/components/landing/Get-More-Week";
 import { IoStatsChartSharp } from "react-icons/io5";
@@ -11,7 +11,7 @@ function Page() {
       <Sidebar />
 
       <main className="flex-1 xl:ml-[480px]">
-        <HeroSection />
+        <CarouselBanner />
         <SharedText
           variant="up"
           text="Get more done in a week"
@@ -24,11 +24,22 @@ function Page() {
         <div className="flex justify-center items-center mt-5 ">
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
             <GetMoreWeek text="Double Your Productivity" icon="2x" />
-            <GetMoreWeek text="Efficiency Increase Per Transfer" icon={<IoStatsChartSharp />} />
-            <GetMoreWeek text="Centralize Your Finances" icon={<AiFillDatabase />} />
+            <GetMoreWeek
+              text="Efficiency Increase Per Transfer"
+              icon={<IoStatsChartSharp />}
+            />
+            <GetMoreWeek
+              text="Centralize Your Finances"
+              icon={<AiFillDatabase />}
+            />
             <GetMoreWeek text="More Activity" icon="130%" />
           </div>
         </div>
+        <SharedText
+          variant="up"
+          text="The Most Reliable App"
+          className="mt-[100px]"
+        />
       </main>
     </div>
   );
--- website/src/components/landing/Carousel-Banner.tsx
+++ website/src/components/landing/Carousel-Banner.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-function SharedCarouselBanner() {
+function HeroSection() {
   return (
-    <div>SharedCarouselBanner</div>
-  )
+    <div className="flex justify-center items-center p-4 mt-8">
+      <Image
+        src="/images/4dd369a6b1dd2d9d8c3e5227ef4f51e7f941a6ba.png"
+        alt="hero-section img"
+        width={1350}    
+        height={0}
+        className="max-h-[650px] object-cover rounded-t-3xl"
+      />
+    </div>
+  );
 }
 
-export default SharedCarouselBanner
+export default HeroSection;
